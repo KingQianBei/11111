2da0f3390624f03c54acb147fccc849d227002a3
刘俊宏 <731588013@qq.com>
2016-04-01 15:25:43 +0800

    Initial Commit

---
+++ 1111/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1111
+//
+//  Created by 刘俊宏 on 16/4/1.
+//  Copyright © 2016年 刘俊宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 1111/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  1111
+//
+//  Created by 刘俊宏 on 16/4/1.
+//  Copyright © 2016年 刘俊宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
